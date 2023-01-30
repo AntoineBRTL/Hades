0031fd034407177d37fb26947969adc999c5126a
AntoineBRTL <antoine.bartoli@outlook.fr>
2023-01-30 19:07:14 +0100

    Remove transpiled ts

--- src/classes/CLILogger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CLILogger = void 0;
-class CLILogger {
-    log(message, outline) {
-        console.log(`[${outline}] ${message}`);
-    }
-}
-exports.CLILogger = CLILogger;
